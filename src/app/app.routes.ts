43659e71c432d2d806ef76963b095157c06cdc68
Bancha Datrungpithak <banchad81@gmail.com>
2024-05-04 16:23:42 +0700

    Setup page routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FileUploadComponent } from './file-upload/file-upload.component';
+import { TransactionComponent } from './transaction/transaction.component';
+import { Page404Component } from './page404/page404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'upload', component: FileUploadComponent },
+    { path: 'transaction', component: TransactionComponent },
+    { path: '',   redirectTo: '/transaction', pathMatch: 'full' },
+    { path: '**', component: Page404Component }
+  ];
---
+++ src/app/page404/page404.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-page404',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule],
+  templateUrl: './page404.component.html',
+  styleUrl: './page404.component.scss'
+})
+export class Page404Component {
+
+}
