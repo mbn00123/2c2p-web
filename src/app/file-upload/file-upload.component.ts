911cfc0f2efb534b0c665c7014710eb72df59e4a
Bancha Datrungpithak <banchad81@gmail.com>
2024-05-05 00:25:39 +0700

    Add input file validation

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -19,15 +19,63 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './file-upload.component.html',
   styleUrl: './file-upload.component.scss'
 })
-export class FileUploadComponent implements OnInit {
+export class FileUploadComponent {
+
+  selectedFile: File | null = null;
+  fileTooLarge = false;
+  invalidExtension = false;
+  fileNotSelected = false;
 
   constructor() { }
 
-  ngOnInit() {
+  onFileSelect(event: any) {
+    const file = event.target.files[0];
+    const allowedExtensions = ['csv', 'xml'];
 
+    if (!file) {
+      this.fileNotSelected = true;
+      this.selectedFile = null;
+      return;
+    } else {
+      this.fileNotSelected = false;
+    }
+
+    // Check file size
+    if (file.size > 1024 * 1024) { // 1 MB
+      this.fileTooLarge = true;
+      this.selectedFile = null;
+      return;
+    } else {
+      this.fileTooLarge = false;
+    }
+
+    // Check file extension
+    const fileExtension = file.name.split('.').pop().toLowerCase();
+    if (!allowedExtensions.includes(fileExtension)) {
+      this.invalidExtension = true;
+      this.selectedFile = null;
+      return;
+    } else {
+      this.invalidExtension = false;
+    }
+
+    this.selectedFile = file;
   }
 
-  upload() {
+  onSubmit() {
+    // Check if file is selected
+    if (!this.selectedFile) {
+      this.fileNotSelected = true;
+      if (this.invalidExtension || this.fileTooLarge) {
+        this.fileNotSelected = false;
+      }
+      return;
+    } else {
+      this.fileNotSelected = false;
+    }
 
+    // Handle file submission
+    console.log("File submitted:", this.selectedFile);
   }
+
 }
